25f59775fcf79006a005d47b85a2021475cf7d1e
Dmitriy Potter <dmitriypottero@gmail.com>
2023-05-04 21:37:04 +0500

    removing keyboard after last message

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def validate_date(date_text, update: Update, context: ContextTypes.DEFAULT
 async def validate_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.text == "Yes":
         print("Operation successful!")
-        await update.message.reply_text("Thank you for using our bot. The information is going to be saved")
+        await update.message.reply_text("Thank you for using our bot. The information is going to be saved", reply_markup=ReplyKeyboardRemove())
         insert_employee(user["id"], user["name"], user["birth"], user["location"], user["profession"])
         return ConversationHandler.END
     else:
